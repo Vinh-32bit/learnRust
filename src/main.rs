f6283f102840e81af7b829d8f90cc8a31c2f371a
Vinh-32bit <vaonguyen27@gmail.com>
2024-11-29 08:04:52 +0700

    up

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello world");
+    println!("{} ages", 10);
+    println!("{1}, age: {0}", 19, "Vinh");
+    println!("{name}, age: {age}, height: {h}",
+            name = "NT Vinh",
+            age = 19,
+            h = 170);
+}
+
+
