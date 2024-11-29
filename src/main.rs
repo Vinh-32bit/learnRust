31f13c9f20e22cfc102a4ba8d9dc0eef8504859d
Vinh-32bit <vaonguyen27@gmail.com>
2024-11-29 08:27:38 +0700

    formatted print

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,26 @@ fn main() {
             name = "NT Vinh",
             age = 19,
             h = 170);
-}
+
+    //co the format bang cach dung :
+
+    println!("Base 10:                  {}",  100);
+    println!("Base 2(binary):           {:b}",100);
+    println!("Base 8(octal):            {:o}",100);
+    println!("Base 16(hexadecimal):     {:x}",100);
+
+    //chen va can le
+    println!("{number:>5}", number=1);
+    println!("{number:0>5}", number=1);
+    println!("{number:0<5}", number=1);
+    //witdh lan *
+    println!("{number:*>width$}", number=1, width=6);
+
+    #[allow(dead_code)]
+    struct Str(i32);
+    let num: f64 = 1.0;
+    let wdth: usize = 20;
+    println!("{num:>wdth$}");
+    }
 
 
